b4d927d9ef037cd63afbe309b99cf5ecb124bb04
Bo van den Berg <b.vandenberg-6@student.tudelft.nl>
2025-09-03 16:09:07 +0200

    Add lifecycle methods

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,15 +1,21 @@
 package com.luv2code.junitdemo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
 
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+    }
+
     @Test
     void testEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
-
         assertEquals(6, demoUtils.add(2, 4), "2 + 4 should equal 6");
         assertNotEquals(8, demoUtils.add(2, 4), "2 + 4 should not equal 8");
         assertNotEquals(6, demoUtils.add(2, 3), "2 + 3 should not equal 6");
@@ -17,8 +23,6 @@ class DemoUtilsTest {
 
     @Test
     void testNullAndNotNull() {
-        DemoUtils demoUtils = new DemoUtils();
-
         String str2 = "luv2code";
 
         assertNull(demoUtils.checkNull(null), "Object should be null");
