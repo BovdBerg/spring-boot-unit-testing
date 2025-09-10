2d8f9c105aedc16cbbfa6b8d72cd2e14c1ef5c75
Bo van den Berg <b.vandenberg-6@student.tudelft.nl>
2025-09-10 14:04:02 +0200

    Add setup with Springboot and properties file

--- 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
+++ 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -1,12 +1,51 @@
 package com.luv2code.test;
 
 import com.luv2code.component.MvcTestingExampleApplication;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
 class ApplicationExampleTest {
-    
+
+    private static int count = 0;
+
+    @Value("${info.app.name}")
+    private String appInfo;
+
+    @Value("${info.app.description}")
+    private String appDescription;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
+    @Value("${info.school.name}")
+    private String schoolName;
+
+    @Autowired
+    CollegeStudent student;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+    @BeforeEach
+    void beforeEach() {
+        count++;
+        System.out.println("Testing: " + appInfo + " which is " + appDescription +
+                ". Version: " + appVersion + ". Execution count: " + count);
+        student.setFirstname("Eric");
+        student.setLastname("Roby");
+        student.setEmailAddress("eric.roby@luv2code_school.com");
+        studentGrades.setMathGradeResults(List.of(100.0, 85.0, 76.5, 91.75));
+        student.setStudentGrades(studentGrades);
+    }
+
     @Test
     void basicTest() {
 
