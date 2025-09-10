bd4624d9b6568d31376c618ff92cea4e92b6a6b5
Bo van den Berg <b.vandenberg-6@student.tudelft.nl>
2025-09-10 16:40:46 +0200

    Setup ReflectionTestUtils test file

---
+++ 2.20-starting-project/src/test/java/com/luv2code/component/ReflectionTestUtilsTest.java
@@ -0,0 +1,40 @@
+package com.luv2code.component;
+
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ReflectionTestUtilsTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired
+    CollegeStudent studentOne;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+    @BeforeEach
+    void beforeEach() {
+        studentOne.setFirstname("Eric");
+        studentOne.setLastname("Roby");
+        studentOne.setEmailAddress("eric.roby@luv2code_school.com");
+        studentOne.setStudentGrades(studentGrades);
+
+        // set private fields via ReflectionTestUtils
+        ReflectionTestUtils.setField(studentOne, "id", 1);
+        ReflectionTestUtils.setField(studentOne, "studentGrades",
+                new StudentGrades(List.of(100.0, 85.0, 76.5, 91.75)));
+    }
+}
