8aef32269dc7ea6e5303ce7f6c8d2ddd5363f4e9
Bo van den Berg <b.vandenberg-6@student.tudelft.nl>
2025-09-10 10:28:15 +0200

    Add tests that loop through multiple (Parameterized) input values

--- 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -48,5 +51,48 @@ class FizzBuzzTest {
         String expected = Integer.toString(input);
 
         assertEquals(expected, FizzBuzz.compute(input), "7 should return 7");
+    }
+
+    @Test
+    @DisplayName("Manually test multiple values")
+    @Order(5)
+    void testValuesFrom1To7() {
+        String[] expectedValues = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7"};
+
+        for (int i = 1; i <= 7; i++) {
+            assertEquals(expectedValues[i - 1], FizzBuzz.compute(i), i + " should return " + expectedValues[i - 1]);
+        }
+    }
+
+    @ParameterizedTest(name = "input={0} --> expected={1}")
+    @CsvSource({
+            "1, 1",
+            "2, 2",
+            "3, Fizz",
+            "4, 4",
+            "5, Buzz",
+            "6, Fizz",
+            "7, 7",
+            "8, 8",
+            "9, Fizz",
+            "10, Buzz",
+            "11, 11",
+            "12, Fizz",
+            "13, 13",
+            "14, 14",
+            "15, FizzBuzz"
+    })
+    @DisplayName("Parameterized test from CSV")
+    @Order(6)
+    void parameterizedTestFromCsv(int input, String expected) {
+        assertEquals(expected, FizzBuzz.compute(input), input + " should return " + expected);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/fizzbuzz-test-data.csv", numLinesToSkip = 1)
+    @DisplayName("Parameterized test from CSV file")
+    @Order(7)
+    void parameterizedTestFromCsvFile(int input, String expected) {
+        assertEquals(expected, FizzBuzz.compute(input), input + " should return " + expected);
     }
 }
