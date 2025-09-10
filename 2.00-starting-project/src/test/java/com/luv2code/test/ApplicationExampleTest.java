f13704e71760efa59e9aa7dad8c9c43147e349dc
Bo van den Berg <b.vandenberg-6@student.tudelft.nl>
2025-09-10 12:16:42 +0200

    Use class from different folder structure

--- 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
+++ 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -1,9 +1,10 @@
-package com.luv2code.component;
+package com.luv2code.test;
 
+import com.luv2code.component.MvcTestingExampleApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
 class ApplicationExampleTest {
     
     @Test
