14347f4d24dfc9538a5a5dede2bbc954a0a22b66
Bo van den Berg <b.vandenberg-6@student.tudelft.nl>
2025-09-11 15:35:08 +0200

    Implement math grade creation

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
@@ -1,7 +1,11 @@
 package com.luv2code.springmvc.service;
 
 import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.models.MathGrade;
+import com.luv2code.springmvc.repository.MathGradesDao;
 import com.luv2code.springmvc.repository.StudentDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,8 +17,14 @@ public class StudentAndGradeService {
 
     private final StudentDao studentDao;
 
-    public StudentAndGradeService(StudentDao studentDao) {
+    private final MathGrade mathGrade;
+
+    private final MathGradesDao mathGradesDao;
+
+    public StudentAndGradeService(StudentDao studentDao, @Qualifier("mathGrades") MathGrade mathGrade, MathGradesDao mathGradesDao) {
         this.studentDao = studentDao;
+        this.mathGrade = mathGrade;
+        this.mathGradesDao = mathGradesDao;
     }
 
     public void createStudent(String firstName, String lastName, String email) {
@@ -39,6 +49,18 @@ public class StudentAndGradeService {
     }
 
     public boolean createGrade(double grade, int studentId, String gradeType) {
+        if (!checkIfStudentIsNull(studentId)) {
+            return false;
+        }
+
+        if (grade >= 0 && grade <= 100 && gradeType.equals("math")) {
+            mathGrade.setId(0);
+            mathGrade.setGrade(grade);
+            mathGrade.setStudentId(studentId);
+            mathGradesDao.save(mathGrade);
+            return true;
+        }
+
         return false;
     }
 }
