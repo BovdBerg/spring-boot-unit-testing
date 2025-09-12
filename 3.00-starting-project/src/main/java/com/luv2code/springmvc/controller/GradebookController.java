7db3d86e747982e533f439ae8019be1a3f03444a
Bo van den Berg <b.vandenberg-6@student.tudelft.nl>
2025-09-12 11:01:57 +0200

    StudentInformation upgrades
    - Return "error" if student does not exist
    - Add student + grade averages to the model

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -46,7 +46,37 @@ public class GradebookController {
 	}
 
 	@GetMapping("/studentInformation/{id}")
-		public String studentInformation(@PathVariable int id, Model m) {
+	public String studentInformation(@PathVariable int id, Model m) {
+		if (!studentAndGradeService.checkIfStudentIsNull(id)) {
+			return "error";
+		}
+
+		GradebookCollegeStudent student = studentAndGradeService.studentInformation(id);
+		m.addAttribute("student", student);
+
+		// Add grade averages to the model
+		if (!student.getStudentGrades().getMathGradeResults().isEmpty()) {
+			m.addAttribute("mathAverage", student.getStudentGrades().findGradePointAverage(
+					student.getStudentGrades().getMathGradeResults())
+			);
+		} else {
+			m.addAttribute("mathAverage", "N/A");
+		}
+		if (!student.getStudentGrades().getScienceGradeResults().isEmpty()) {
+			m.addAttribute("scienceAverage", student.getStudentGrades().findGradePointAverage(
+					student.getStudentGrades().getScienceGradeResults())
+			);
+		} else {
+			m.addAttribute("scienceAverage", "N/A");
+		}
+		if (!student.getStudentGrades().getHistoryGradeResults().isEmpty()) {
+			m.addAttribute("historyAverage", student.getStudentGrades().findGradePointAverage(
+					student.getStudentGrades().getHistoryGradeResults())
+			);
+		} else {
+			m.addAttribute("historyAverage", "N/A");
+		}
+
 		return "studentInformation";
 	}
 }
--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -151,4 +151,34 @@ class GradebookControllerTest {
         assert mav != null;
         ModelAndViewAssert.assertViewName(mav, "error");
     }
+
+    @Test
+    void studentInformationHttpRequest() throws Exception {
+        assertTrue(studentDao.findById(1).isPresent());
+
+        MvcResult mvcResult = mockMvc.perform(
+                        MockMvcRequestBuilders.get("/studentInformation/{id}", 1))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        assert mav != null;
+        ModelAndViewAssert.assertViewName(mav, "studentInformation");
+    }
+
+    @Test
+    void studentInformationNonExistentStudentHttpRequest() throws Exception {
+        assertFalse(studentDao.findById(0).isPresent());
+
+        MvcResult mvcResult = mockMvc.perform(
+                        MockMvcRequestBuilders.get("/studentInformation/{id}", 0))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        assert mav != null;
+        ModelAndViewAssert.assertViewName(mav, "error");
+    }
 }
